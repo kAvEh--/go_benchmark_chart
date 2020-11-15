5da24095ec4479bcaf456dbb77499f371fa6be97
kaveh fazaeli <kaveh.fazaeli@gmail.com>
2020-11-15 20:03:37 +0330

    Add google chart and some sample data

--- main_test.go
+++ main_test.go
@@ -6,15 +6,20 @@ import (
 )
 
 func BenchmarkFib(b *testing.B) {
-	for _, size := range []int{1, 2, 3, 10, 20, 40} {
+	for _, size := range []int{1, 2, 3, 10, 20, 40, 60} {
 		benchmarkFib(b, size)
 	}
 }
 
+var result int
+
 func benchmarkFib(b *testing.B, size int) {
 	b.Run(fmt.Sprintf("Fib_%d", size), func(b *testing.B) {
+		var r int
 		for i := 0; i < b.N; i++ {
-			_ = Fib(size)
+			r = Fib(size)
 		}
+
+		result = r
 	})
 }
